a2534a8797c565a9beb3b5151cecdbb6c8e7cd5c
DylanM200 <63425277+DylanM200@users.noreply.github.com>
2020-04-10 15:59:13 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kie1C79JubruuuPd_cZBVVIhJ8nB331XhGyjjU8Bqro/edit#gid=0';
